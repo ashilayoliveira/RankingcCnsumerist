32a016173bfc24f09e5a7d865cac4eefbdb1ab37
Ashilay de Oliveira <142242769+ashilayoliveira@users.noreply.github.com>
2023-12-01 22:33:14 -0300

    Add files via upload

--- Bootcamp Python/index.py
+++ Bootcamp Python/index.py
@@ -5,12 +5,12 @@ def calcular_pontos(vazamento):
         pontos += pesos.get(dado, 0) * valor
     return pontos
 
-def criar_rank(vazamentos):
+def rank_Consum(vazamentos):
     rank = sorted(vazamentos.items(), key=lambda x: calcular_pontos(x[1]), reverse=True)
     return rank
 
 def obter_dados():
-    site = input("Digite o nome do site (Shopee, Mercado Livre, Amazon): ")
+    site = input("Digite o nome do site (Shopee, Mercado Livre, Shein): ")
     dados = {}
     while True:
         dado = input("Digite o tipo de dado vazado (Senha, Email, Telefone, Nome) ou 'exit' para encerrar: ").capitalize()
@@ -26,12 +26,10 @@ def obter_dados():
 def main():
     vazamentos = {}
 
-    # Simula a entrada de dados para Shopee, Mercado Livre e Amazon
     for _ in range(3):
         site, dados = obter_dados()
         vazamentos[site] = dados
 
-    # Cria o ranking com base nos vazamentos e exibe
     rank = criar_rank(vazamentos)
 
     print("\nRanking Decrescente:")
